3bf3ba7f5c2cf8ea63e77e5031b899e495e6fe35
Chuck <wangjohn419@gmail.com>
2026-08-29 19:42:46 -0700

    Remove legacy download_model_pipeline package

--- src/download_model_pipeline/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Hugging Face -> HDFS model ingestion pipeline."""
-
-from .config import DownloadModelConfig
-from .task import download_model
-
-__all__ = ["DownloadModelConfig", "download_model"]
