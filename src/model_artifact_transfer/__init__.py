104a23ba01d43929f795a824dc1079c2c883a125
Chuck <wangjohn419@gmail.com>
2026-09-10 15:30:13 -0700

    Export S3 destination

--- src/model_artifact_transfer/__init__.py
+++ src/model_artifact_transfer/__init__.py
@@ -1,13 +1,13 @@
 """Portable model-artifact transfer primitives."""
 
 from .core import ArtifactDestination, ArtifactSource, transfer_artifact
-from .hdfs import HdfsDestination
 from .huggingface import HuggingFaceSource
+from .s3 import S3Destination
 
 __all__ = [
     "ArtifactDestination",
     "ArtifactSource",
-    "HdfsDestination",
     "HuggingFaceSource",
+    "S3Destination",
     "transfer_artifact",
 ]
